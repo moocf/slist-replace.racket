2c670239212154fcfda6dd22d2ee22ca3ef4d6ca
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 15:49:50 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,21 +1,21 @@
 #lang racket
 
-;; subst: Symbol x Symbol x SList -> SList
-;; usage: (subst new old slist) = substitute symbol in a symbol list
-(define subst
+;; slist.replace: Symbol x Symbol x SList -> SList
+;; usage: (slist.replace new old slist) = substitute symbol in a symbol list
+(define slist.replace
   (lambda (new old slist)
     (if (null? slist)
         '()
-        (cons (subst-sexp new old (car slist))
-              (subst new old (cdr slist))))))
+        (cons (slist.replace-sexp new old (car slist))
+              (slist.replace new old (cdr slist))))))
 
-(define subst-sexp
+(define slist.replace-sexp
   (lambda (new old sexp)
     (if (not (list? sexp))
         (if (eqv? old sexp)
             new
             sexp)
-        (subst new old sexp))))
+        (slist.replace new old sexp))))
 
 ; s-list ::= ({s-exp}*)
 ; s-exp  ::= symbol | s-list
